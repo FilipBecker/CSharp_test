9f2fa54e9266365a342b3d2e33681653110aae9b
FilipBecker <filip.becker@edu.huddinge.se>
2024-05-28 09:20:29 +0200

    Commit to save

---
+++ CsharpProjects/TestProject/Program.cs
@@ -0,0 +1,21 @@
+﻿Random random = new Random();
+int monsterH = 10;
+int heroH = 10;
+int damege;
+
+do {
+    damege = random.Next(1, 11);
+    monsterH -= damege;
+    Console.WriteLine($"Monster({monsterH}) takes {damege} points of damege");
+    if (monsterH > 0) {
+        damege = random.Next(1, 11);
+        heroH -= damege;
+        Console.WriteLine($"Hero({heroH}) takes {damege} points of damege");
+    }
+} while ((heroH > 0) && (monsterH > 0));
+
+if (monsterH <= 0) {
+    Console.WriteLine("Hero won");
+} else {
+    Console.WriteLine("Monster won");
+}
